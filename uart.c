8bae30dab885c247fd4279dfff3e41cdb3008488
Luciano-P <luciano.peschiutta@mi.unc.edu.ar>
2021-10-29 12:03:50 -0300

    Un par de funciones para ir probando UART

---
+++ uart.c
@@ -0,0 +1,25 @@
+
+
+#include "LPC17xx.h"
+#include "lpc17xx_uart.h"
+
+void conf_UART(void)
+{
+
+	UART_CFG_Type uartconf;
+
+	uartconf.Baud_rate = 9600;
+	uartconf.Parity = UART_PARITY_ODD;
+	uartconf.Databits = UART_DATABIT_8;
+	uartconf.Stopbits = UART_STOPBIT_1;
+
+	UART_Init(LPC_UART0, uartconf);
+
+}
+
+void send_UART(uint8_t x)
+{
+
+	UART_SendByte(LPC_UART0, x);
+
+}
