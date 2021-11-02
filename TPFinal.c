713e3de20cab795c0077156a83cad8e45f8daeb2
Luciano-P <luciano.peschiutta@mi.unc.edu.ar>
2021-11-02 09:44:26 -0300

    Eliminado el archivo uart.c y distribuido entre el main y configuraciones.c Header configuraciones.h redactado correctamente

--- TPFinal.c
+++ TPFinal.c
@@ -4,6 +4,12 @@
  * Integrantes: Luciano Peschiutta, Teo Argañaraz
  */
 
+
+/*
+ * Configuracion de pines de la placa:
+ *
+ */
+
 #include "LPC17xx.h"
 
 #include "configuraciones.h"
@@ -122,6 +128,22 @@ void ADC_IRQHandler(void)
 		nr_muestra ++;											//Incremento el contador de muestras
 
 	}
+
+}
+
+
+/*
+ * Toma la conversion de 12 bits del ADC y la divide en 2 bytes para luego enviarla mediante UART0
+ */
+void send_UART_12b(uint16_t palabra)
+{
+
+	uint8_t aux;
+
+	aux = palabra & 0xFF;
+	UART_SendByte(LPC_UART0, aux);
+	aux = (palabra>>8) & 0xF;
+	UART_SendByte(LPC_UART0, aux);
 
 }
 
--- configuraciones.h
+++ configuraciones.h
@@ -1,8 +1,14 @@
 
 
+#ifndef _CONFIGURACIONES_TP
+#define _CONFIGURACIONES_TP
+
 void conf_gpio(void);
 void conf_tim0_g(void);
 void conf_tim0_r(void);
 void conf_tim1(int ms);
 void conf_ADC(void);
 void conf_DAC(void);
+void conf_UART(void);
+
+#endif
